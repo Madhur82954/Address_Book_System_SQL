3366f89ff41b5274e5d1fe9837a642393243e446
Madhur82954 <itzmadhurverma@gmail.com>
2023-06-28 12:48:35 +0530

    [Madhur] - UC2-CreateTable Added

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,2 +1,6 @@
 --UC1--
 create database AddressBookService;
+
+--UC2--
+create table Address_Book_System(first_name varchar(50),last_name varchar(50),address varchar(200),city varchar(50),state varchar(50),Zip int,Phoneno bigint,Emailid varchar(200));
+select * from Address_Book_System;
